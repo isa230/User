121ab6a313bea5015fd86b4fbc7d0b1b9594cc35
unknown <jorman398@gmail.com>
2020-09-26 16:05:30 +0200

    config de MONGO

--- Datos/config/server.js
+++ Datos/config/server.js
@@ -20,16 +20,6 @@ let urlDB;
 //la condicional no quiere funcionar, es decir el url local no me quiere guardar nada 
 //per me funciona para la nube quiero decir que guarda en el cluster pero todo en desarrollo 
 //no en producion :`(
-/*if (process.env.NODE_ENV === 'dev') {
-
-    urlDB = 'mongodb://localhost:27017/Empleado';
-
-} else {*/
-
-//urlDB = process.env.MONGO_URI;
-//}
-
-//process.env.URLDB = urlDB;
 
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/Empleado";
@@ -38,6 +28,6 @@ if (process.env.NODE_ENV === "dev") {
 }
 process.env.URLDB = urlDB;
 
-module.exports = {
-    port: process.env.PORT
-};
+//module.exports = {
+//  port: process.env.PORT
+//};
